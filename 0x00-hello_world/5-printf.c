4e539b0e1b6fcce5920b4384a5300ad7e2b729d2
Your Namegarykhumalo <lebogangkhumalo282gmail.com>
2023-02-19 05:36:35 -0800

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
  * Return: Always 0 (success)
- * /
-int main(void)
+ */
+ int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	pirintf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
