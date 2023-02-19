22b411bd7c23178ce6d157f3620e9f3bbed307c3
Your Namegarykhumalo <lebogangkhumalo282gmail.com>
2023-02-19 08:32:06 -0800

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (sucess)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
 	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of int: %d byte(s)|n", sizeof(int));
+	printf("Size of an int: %d byte(s)|n", sizeof(int));
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n",sizeof(long long int));
 	printf("size of a float: %d byte(s)\n",sizeof(float));
