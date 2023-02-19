f582d0baeb36710797bf261947835966107f4af7
Lebogangkhumalo <122866881+garykhumalo@users.noreply.github.com>
2023-02-19 15:26:20 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - entry point
  *
@@ -9,4 +9,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
