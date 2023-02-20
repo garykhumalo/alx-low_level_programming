e1da80712d5a989ddbc758ff406650bd619b3318
Lebogangkhumalo <122866881+garykhumalo@users.noreply.github.com>
2023-02-20 03:10:17 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - function
  *
- * Return: 0 (success)
+ * Return: 0 on success
  */
 int main(void)
 {
-	int s = 0, e =9;
+	int s = 0, e = 9;
 
 	while (s <= e)
 	{
