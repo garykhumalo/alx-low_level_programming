81fb8394b221a517d7b6a85491adfc936a805cce
Lebogangkhumalo <122866881+garykhumalo@users.noreply.github.com>
2023-02-23 03:29:22 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,6 @@
-#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 int _putchar(char);
 void print_alphabet(void);
@@ -12,7 +14,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-
 
 #endif
